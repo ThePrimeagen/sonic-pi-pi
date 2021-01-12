7ecd0e0fb2ba12ddd752e12fbbfaa429f653ba59
Michael Paulson <mpaulson@netflix.com>
2021-01-11 19:07:10 -0800

    ntoheunth

--- server/server.py
+++ server/server.py
@@ -5,9 +5,12 @@ import os
 
 async def hello(websocket, path):
     while True:
-        msg = await websocket.recv()
-        print(f"> {msg}", flush=True)
-        os.system(f"echo \"{msg}\" | sonic_pi")
+        try:
+            msg = await websocket.recv()
+            print(f"> {msg}", flush=True)
+            os.system(f"echo \"{msg}\" | sonic_pi")
+        except Exception as e:
+            print(f"Nice try guy {str(e)}")
 
 start_server = websockets.serve(hello, "0.0.0.0", 42069)
 asyncio.get_event_loop().run_until_complete(start_server)
